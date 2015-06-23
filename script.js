497508bc32b2f694ae78255d10c78a6b12056054
tatocaster <kutaliatato@gmail.com>
2015-06-23 22:35:26 +0400

    add Runge Kutta Method

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 /**
-* @author  tatocaster <tatocaster.me>
-*/
-(function(document, window){
+ * @author  tatocaster <tatocaster.me>
+ */
+(function(document, window) {
 
     var selectBox = getById('de_method');
     var finalX = getById('final_valueX');
     var calculateBtn = getById('calculate');
 
-    selectBox.onchange = function(){
-        if(this.value == 2){
+    selectBox.onchange = function() {
+        if (this.value == 2 || this.value == 3) {
             finalX.style.display = "block";
-        }else{
+        } else {
             finalX.style.display = "none";
         }
     };
 
-    calculate.onclick = function(){
+    calculate.onclick = function() {
         var k = 0;
         var n = 10;
         var y = parseFloat(getById("yzero").value);
@@ -67,7 +67,23 @@
             console.log("The respective values of xArr and yArr are\n     xArr yArr");
             for (j = 0; j < i; j++) {
                 // answer at last. Aaah.
-                tablePrint += "<tr><td>" + (j+1) + "</td><td>" +xArr[j] + "</td><td>" + yArr[j] + "</td></tr>";
+                tablePrint += "<tr><td>" + (j + 1) + "</td><td>" + xArr[j] + "</td><td>" + yArr[j] + "</td></tr>";
+            }
+        }
+
+        if (selectBox.value == 3) {
+            var fX = finalX.value;
+            var i = 0;
+            while (x < fX) {
+                m1 = eulerFunction(x, y);
+                m2 = eulerFunction((x + h / 2), (y + m1 * h / 2));
+                m3 = eulerFunction((x + h / 2), (y + m2 * h / 2));
+                m4 = eulerFunction((x + h), (y + m3 * h));
+                m = ((m1 + 2 * m2 + 2 * m3 + m4) / 6);
+                y = y + m * h;
+                x = x + h;
+                i++;
+                tablePrint += "<tr><td>" + i + "</td><td>" + Math.round(x * 100) / 100 + "</td><td>" + Math.round(y * 1000) / 1000 + "</td></tr>";
             }
         }
 
@@ -75,7 +91,10 @@
     };
 
 
-
+    function eulerFunction(x, y) {
+        m = (x - y) / (x + y);
+        return m;
+    }
 
     function calcSlope(a, b) {
         var c;
@@ -83,7 +102,7 @@
         return c;
     }
 
-    function getById(id){
+    function getById(id) {
         return document.getElementById(id);
     }
 
